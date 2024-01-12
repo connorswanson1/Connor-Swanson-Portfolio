8692556a5095af986832486440d3b66a5de7cc7e
connorswanson1 <connorswanson13@gmail.com>
2024-01-11 20:42:41 -0700

    About Page updates

--- my-portfolio/src/pages/About.js
+++ my-portfolio/src/pages/About.js
@@ -1,19 +1,30 @@
-//About.js
 import React from 'react';
+import profilePhoto from '../assets/sunset_portrait.jpg'; // Path to your profile photo
 
 function About() {
     return (
-        <div className="about-container">
-            <h1>About Me</h1>
-            <p>
-                Hi, I'm Connor Swanson, a passionate software engineer with a love for developing
-                scalable web applications and working on challenging problems.
-            </p>
-            <p>
-                I specialize in full-stack development, with a particular interest in using
-                JavaScript frameworks like React.js to create user-friendly interfaces.
-            </p>
-            {/* Add more content about your skills, experience, etc. */}
+        <div className="about-page">
+            <div className="about-content">
+                <img src={profilePhoto} alt="Your Name" className="about-profile-photo" />
+                <div className="about-text">
+                    <h1>I'm Connor Swanson, a software engineer</h1>
+                    <p>
+                        In the fall of 2023 I graduated from the University of Northern Colorado, with a bachelor's
+                        degree in Software Engineering. Now, I am eager to get my start in the ever-changing and
+                        fast moving world of tech.
+                    </p>
+                    <p>
+                        4 Years of college computer science courses have given me a well rounded skill set,
+                        ranging from ...
+                    </p>
+                    <p> Outside of tech, you can usually find me riding my skateboard, a hobby I have been
+                        practicing for more than ten years now! And when its too cold for that, I love staying
+                        in and playing games with my buddies. My weekends are usually spent with my wonderful
+                        girlfriend, going to concerts, seeing movies at old theaters, or hunting for records.
+                    </p>
+                    {/* You can add more paragraphs or sections as needed */}
+                </div>
+            </div>
         </div>
     );
 }
