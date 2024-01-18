31e9ad2c2b8e4d649ec005cf0a7300fe22db84eb
connorswanson1 <connorswanson13@gmail.com>
2024-01-18 16:42:40 -0700

    Project page update

--- my-portfolio/src/components/ProjectCard.js
+++ my-portfolio/src/components/ProjectCard.js
@@ -4,12 +4,36 @@ import '../styles/ProjectCard.css'; // CSS for individual project cards
 function ProjectCard({ project }) {
     return (
         <div className="project-card">
-            <img src={project.imageUrl} alt={project.title} className="project-image" />
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            {/* More details or a link to a detailed view */}
+            <div className="project-text">
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                {/* Embed YouTube video if available, if not use photo */}
+                {project.videoUrl ? (
+                    <div className="video-container">
+                        <iframe width="560"
+                            height="315"
+                            src={project.videoUrl}
+                            title="YouTube video player"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen>
+                        </iframe>
+                    </div>
+                ) : (
+                    project.imageUrl && (
+                        <img src={project.imageUrl} alt={project.title} className="project-image" />
+                    )
+                )}
+                {project.githubUrl && (
+                    <div className="project-github-link">
+                        <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">View on GitHub</a>
+                    </div>
+                )}
+            </div>
+
         </div>
     );
 }
+
 
 export default ProjectCard;
--- my-portfolio/src/pages/Projects.js
+++ my-portfolio/src/pages/Projects.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import ProjectCard from '../components/ProjectCard'; // Assume you have a ProjectCard component
 import '../styles/Projects.css'; // CSS file for styling
+import proj1Photo from '../assets/gtt_ss.png';
 
 function Projects() {
     // Replace this with actual project data
     const projects = [
         {
             id: 1,
-            title: 'Project One',
-            description: 'Brief description of Project One...',
-            imageUrl: 'url-to-image',
-            // ... other project details
-        },
-        {
-            id: 2,
-            title: 'Project Two',
-            description: 'Brief description of Project Two...',
-            imageUrl: 'url-to-image',
-            // ... other project details
-        },
-        {
-            id: 3,
-            title: 'Project Three',
-            description: 'Brief description of Project Three...',
-            imageUrl: 'url-to-image',
-            // ... other project details
+            title: 'Gamified Task Tracker',
+            description: 'iOS and Android App used for administering, tracking, and completing tasks.',
+            imageUrl: proj1Photo,
+            videoUrl: 'https://www.youtube.com/embed/n6FNI6N8nVw?si=DTipHvPzAKcCydYg',
+            githubUrl: 'https://github.com/thejacobhardman/Gamified-Task-Tracker',
         },
         // ... more projects
     ];
